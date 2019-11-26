4194a773609354446493e806bb66d25529b26ec7
fkhangitazlogin <54518629+fkhangitazlogin@users.noreply.github.com>
2019-11-26 16:27:05 +0530

    Update ValuesController.cs

--- src/simpleapi/Controllers/ValuesController.cs
+++ src/simpleapi/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace simpleapi.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Vardhan", "API App Test" };
         }
 
         // GET api/values/5
